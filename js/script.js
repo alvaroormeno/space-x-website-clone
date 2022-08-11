240abd0fea9789760640f0d3d075730a19ed54e2
alvaroormeno <alvaro@ormeno.org>
2022-08-10 23:05:31 -0400

    wired counter to js file, created function to update counter to target value. working and completE

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu')
+const counters = document.querySelectorAll('.counter')
 
 btn.addEventListener('click', navToggle);
 
@@ -11,4 +12,35 @@ function navToggle() {
   document.body.classList.toggle('stop-scrolling')
   menu.classList.toggle('show-menu')
 }
+
+function countUp(){
+  counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      // Get count target from data-target attribute
+      const target = Number(counter.getAttribute('data-target'));
+      // Get current counter value // Instead of wrapping with Number() we can just add + sign in front to conver value into number
+      const c = +counter.innerText;
+
+      //Create an increment
+      const increment = target / 100;
+      console.log(increment)
+    
+      // If counter is less than target, add increment
+      if(c < target) {
+        //round up to whole number and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`
+        
+        // calls again updateCounter function every 75 miliseconds only until c < target
+        setTimeout(updateCounter, 75);
+      } 
+      
+    }
+
+    updateCounter()
+  });
+}
+
+countUp()
 
