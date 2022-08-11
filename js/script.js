12cc07aca5a55f5a57bf979d2c0852579e27f7a6
alvaroormeno <alvaro@ormeno.org>
2022-08-10 20:10:21 -0400

    dark overlay after clicking hamburger menu working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', navToggle);
 
 function navToggle() {
   btn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show')
 }
 
-console.log(btn)
